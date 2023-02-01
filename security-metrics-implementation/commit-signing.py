cca8b2fbb4d2a4087121c6f456c84b1d853c9b3f
Imranur Rahman <ir.shimul@gmail.com>
2023-02-01 01:22:39 -0500

    Access token is stored in an environment variable.

--- security-metrics-implementation/commit-signing.py
+++ security-metrics-implementation/commit-signing.py
@@ -1,12 +1,17 @@
 import os
+from dotenv import load_dotenv
 from github import Github
 
 NUMBER_OF_COMMITS_TO_CHECK = 30
 
+def load_access_token_in_env():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
 def authenticate_user():
     # Reading the personal access token from os environ
-    access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
-
+    access_token = os.environ["GITHUB_ACCESS_TOKEN"]
+    # print (access_token)
     client = Github(login_or_token=access_token)
     user = client.get_user()
     # print (user.name)
@@ -27,6 +32,7 @@ def check_code_signing(client, repo_name):
 
 
 if __name__ == "__main__":
+    load_access_token_in_env()
     client = authenticate_user()
     # signed_commits = check_code_signing(client=client, repo_name="PyGithub/PyGithub")
     signed_commits = check_code_signing(client=client, repo_name="imranur-rahman/risk-explorer-for-software-supply-chains")
