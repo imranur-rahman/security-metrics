4e59b3b7788f381e89610d513fd26d5ed197913e
Imranur Rahman <ir.shimul@gmail.com>
2023-02-01 21:21:12 -0500

    Time calculation added for checking if signed_commit is enabled.

--- security-metrics-implementation/commit-signing.py
+++ security-metrics-implementation/commit-signing.py
@@ -1,3 +1,4 @@
+import time
 import os
 from dotenv import load_dotenv
 from github import Github
@@ -18,10 +19,10 @@ def authenticate_user():
     return client
 
 def check_code_signing(client, repo_name):
-    print(client.get_user().login)
+    # print(client.get_user().login)
     repo = client.get_repo(repo_name)
     commits = repo.get_commits().get_page(0) # 0-th page already contains latest 30 commits
-    print (len(commits))
+    # print (len(commits))
     assert len(commits) == NUMBER_OF_COMMITS_TO_CHECK
     # If all commits are signed return True
     ret = True
@@ -31,11 +32,27 @@ def check_code_signing(client, repo_name):
         ret &= commit.commit.raw_data["verification"]["verified"]
     return ret
 
+def check_signed_commit_with_time(client, repo_name):
+    # Starting time
+    st = time.time()
+    is_signed_commit_enabled = check_code_signing(client=client, repo_name=repo_name)
+    # Ending time
+    et = time.time()
+    return is_signed_commit_enabled, (et - st)
+
 
 if __name__ == "__main__":
     load_access_token_in_env()
     client = authenticate_user()
-    # signed_commits = check_code_signing(client=client, repo_name="PyGithub/PyGithub")
-    signed_commits = check_code_signing(client=client, repo_name="chromium/chromium")
-    # signed_commits = check_code_signing(client=client, repo_name="imranur-rahman/risk-explorer-for-software-supply-chains")
-    print(f"signed_commit: {signed_commits}")
+
+    repo = "PyGithub/PyGithub"
+    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
+    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
+    
+    repo = "chromium/chromium"
+    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
+    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
+    
+    repo = "imranur-rahman/risk-explorer-for-software-supply-chains"
+    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
+    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
