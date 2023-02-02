ad3da1b9edc02b07d2eea7e2d4ad2d0d3f819a5b
Imranur Rahman <ir.shimul@gmail.com>
2023-02-02 12:20:32 -0500

    Demo csv file generated for repo, signed_commit status and time needed for this calculation.

--- security-metrics-implementation/commit-signing.py
+++ security-metrics-implementation/commit-signing.py
@@ -1,3 +1,4 @@
+import csv
 import time
 import os
 from dotenv import load_dotenv
@@ -11,24 +12,23 @@ def load_access_token_in_env():
 
 def authenticate_user():
     # Reading the personal access token from os environ
-    access_token = os.environ["GITHUB_ACCESS_TOKEN"]
-    # print (access_token)
+    access_token = os.environ.get("GITHUB_ACCESS_TOKEN")
+
+    # To check if the access token is actually in env variable
+    assert access_token != None
+    
     client = Github(login_or_token=access_token)
-    user = client.get_user()
-    # print (user.name)
     return client
 
 def check_code_signing(client, repo_name):
-    # print(client.get_user().login)
     repo = client.get_repo(repo_name)
     commits = repo.get_commits().get_page(0) # 0-th page already contains latest 30 commits
-    # print (len(commits))
-    assert len(commits) == NUMBER_OF_COMMITS_TO_CHECK
+    
+    assert len(commits) <= NUMBER_OF_COMMITS_TO_CHECK
+
     # If all commits are signed return True
     ret = True
     for commit in commits:
-        # print(commit)
-        # print (commit.commit.raw_data["verification"]["verified"])
         ret &= commit.commit.raw_data["verification"]["verified"]
     return ret
 
@@ -40,19 +40,36 @@ def check_signed_commit_with_time(client, repo_name):
     et = time.time()
     return is_signed_commit_enabled, (et - st)
 
+def open_csv_file():
+    # Open CSV file to store the   data
+    csv_file = open("signed_commit_demo.csv", "w")
+    header = ["Repository", "Is Signed Commit Enabled", "Time Elapsed (s)"]
+    writer = csv.DictWriter(csv_file, fieldnames=header)
+    writer.writeheader()
+    return writer
+
+def func(csv_writer, client, repo_name):
+    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
+    csv_writer.writerow({
+        "Repository": repo_name,
+        "Is Signed Commit Enabled": is_signed_commit_enabled,
+        "Time Elapsed (s)": time_elapsed
+    })
 
 if __name__ == "__main__":
     load_access_token_in_env()
     client = authenticate_user()
 
+    csv_writer = open_csv_file()
+
     repo = "PyGithub/PyGithub"
-    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
-    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
+    func(csv_writer=csv_writer, client=client, repo_name=repo)
     
     repo = "chromium/chromium"
-    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
-    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
-    
+    func(csv_writer=csv_writer, client=client, repo_name=repo)
+
     repo = "imranur-rahman/risk-explorer-for-software-supply-chains"
-    is_signed_commit_enabled, time_elapsed = check_signed_commit_with_time(client=client, repo_name=repo)
-    print(f"repo: {repo} \t signed_commit_enabled: {is_signed_commit_enabled}, time_elapsed: {time_elapsed} sec")
+    func(csv_writer=csv_writer, client=client, repo_name=repo)
+
+    repo = "imranur-rahman/dns-cache-poisoning-attack-reloaded"
+    func(csv_writer=csv_writer, client=client, repo_name=repo)
